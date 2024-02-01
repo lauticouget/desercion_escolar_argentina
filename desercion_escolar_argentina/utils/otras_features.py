e387a250480cb33241d2bf0fe6e051145af2948f
jbeiroa <jbeiroa@gmail.com>
2024-02-01 16:02:54 -0300

    otras_features: variables relacionadas al jefe de hogar en script y notebook eda_datos_panel

--- desercion_escolar_argentina/utils/otras_features.py
+++ desercion_escolar_argentina/utils/otras_features.py
@@ -40,7 +40,7 @@ def crear_feature_binaria(df: pd.DataFrame,
     return data
 
 if __name__ = '__main__':
-
+    # dataframe de estudiantes que desertaron en el tercer trimestre de 2021
     hogares21_2 = pyeph.get(data="eph", year=2021, period=2, base_type='hogar')
     individuos21_2 = pyeph.get(data="eph", year=2021, period=2, base_type='individual')
 
@@ -48,7 +48,7 @@ if __name__ = '__main__':
     individuos21_3 = pyeph.get(data="eph", year=2021, period=3, base_type='individual')
 
     cols = ['CODUSU', 'NRO_HOGAR', 'COMPONENTE', 'CH06', 'CH10', 'NIVEL_ED']
-# uno los dataframes por CODUSU, NRO_HOGAR y COMPONENTE
+    # uno los dataframes por CODUSU, NRO_HOGAR y COMPONENTE
     _data = pd.merge(left=individuos21_2[cols],
                       right=individuos21_3[cols],
                       how='inner',
@@ -62,12 +62,45 @@ if __name__ = '__main__':
 
     desertados21_3 = _data.query(f'{cond_edad} & {cond_asistencia} & {cond_desercion} & {cond_nivel}')
 
+    # variables categóricas: JEFE_TRABAJA, JEFA_MUJER, HOGAR_MONOP, CONYUGE_TRABAJA
+
+    # dataframes de jefxs de hogares y cónyuges
     jefxs_mask = individuos21_2.CH03 == 1
-    jefxs = individuos21_2[jefxs_mask].loc[:, ['CODUSU', 'NRO_HOGAR', 'COMPONENTE', 'ESTADO']]
-    estado_jefx = pd.DataFrame(jefxs.groupby(['CODUSU', 'NRO_HOGAR'])['ESTADO'].sum()).reset_index()
-    data = unir_presonas_hogares(desertados21_3, estado_jefx)
+    conyuge_mask = individuos21_2.CH03 == 2
+    jefxs = individuos21_2[jefxs_mask].loc[:, ['CODUSU', 'NRO_HOGAR', 'COMPONENTE', 'ESTADO', 'CH04']]
+    conyuges = individuos21_2[conyuge_mask].loc[:, ['CODUSU', 'NRO_HOGAR', 'COMPONENTE', 'ESTADO', 'CH04']]
+    
+    # hogares por estado de ocupación del jefx
+    jefxs = pd.DataFrame(jefxs.groupby(['CODUSU', 'NRO_HOGAR'])['ESTADO', 'CH04'].sum()).reset_index()
+    # hogares no monoparentales según género y ocupación del cónyuge
+    hogar_no_mono = pd.DataFrame(conyuges.groupby(['CODUSU', 'NRO_HOGAR'])[['ESTADO', 'CH04']].sum()).reset_index()
+    
+    # variables JEFE_TRABAJA y JEFA_MUJER
+    data = unir_presonas_hogares(desertados21_3, jefxs)
     jefe_trabaja_cond = data.ESTADO == 1
     jefe_trabaja = crear_feature_binaria(data, 'JEFE_TRABAJA', jefe_trabaja_cond)
 
+    jefa_mujer_cond = data.CH04 == 2
+    jefa_mujer = crear_feature_binaria(data, 'JEFA_MUJER', jefa_mujer_cond)
+
     print('Estado de ocupación del jefx de hogar de las personas que dejaron sus estudios entre el segundo y tercer trimestre de 2021.')
     jefe_trabaja.JEFE_TRABAJA.value_counts(normalize=True)
+
+    print('Distribución de desertados según género del jefx de hogar.')
+    jefa_mujer.JEFA_MUJER.value_counts(normalize=True)
+
+    # HOGAR_MONOP
+    data = unir_presonas_hogares(desertados21_3, hogar_no_mono)
+    cond_hogar_monop = df[['ESTADO']].isna()
+    hogar_monop = crear_feature_binaria(data, 'HOGAR_MONOP', cond_hogar_monop)
+
+    print('Distribución de desertados según hogar monoparental o no.')
+    hogar_monop.HOGAR_MONOP.value_counts(normalize=True)
+
+    # variable CONYUGE_TRABAJA
+    data = unir_presonas_hogares(desertados21_3, hogar_no_mono)
+    cond_conyuge_trabaja = data.ESTADO == 1
+    conyuge_trabaja = crear_feature_binaria(data, 'CONYUGE_TRABAJA', cond_conyuge_trabaja)
+
+    print('Distribución de desertados según condición laboral del cónyuge de hogar.')
+    hogar_monop.HOGAR_MONOP.value_counts(normalize=True)

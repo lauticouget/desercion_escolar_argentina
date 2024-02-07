5454c22df096017a73efd352a15104640890d5be
jbeiroa <jbeiroa@gmail.com>
2024-02-07 17:57:29 -0300

    construir_dataset: agrego columnas para calcular variables nbi

--- desercion_escolar_argentina/utils/limpieza.py
+++ desercion_escolar_argentina/utils/limpieza.py
@@ -1,23 +1,24 @@
 import pandas as pd
 
 cols_id_hogar = [
-    'CODUSU', 'NRO_HOGAR', 'REALIZADA', 'ANO4', 'TRIMESTRE', 'REGION','MAS_500', 'AGLOMERADO', 'PONDERA'
+    'CODUSU', 'NRO_HOGAR', 'REALIZADA', 'ANO4', 'TRIMESTRE', 'REGION', 'MAS_500', 'AGLOMERADO', 'PONDERA'
 ]
 
 cols_hogar = [
-    'IV1', 'IV2', 'IV3', 'IV4', 'IV5', 'IV6', 'IV7', 'IV8', 'IV9', 'IV10','IV11', 'IV12_1', 'IV12_2', 'IV12_3', 'II1', 'II2', 'II3','II4_1', 'II4_2', 'II4_3', 'II7', 'II8', 'II9', 'V1', 'V2', 'V21', 'V22', 'V3', 'V5', 'V6', 'V7', 'V8', 'V11', 'V12', 'V13','V14', 'IX_TOT', 'IX_MEN10','IX_MAYEQ10', 'ITF', 'DECCFR'
+    'IV1', 'IV2', 'IV3', 'IV4', 'IV5', 'IV6', 'IV7', 'IV8', 'IV9', 'IV10', 'IV11', 'IV12_1', 'IV12_2', 'IV12_3', 'II1', 'II2', 'II3', 'II4_1', 'II4_2', 'II4_3', 'II7', 'II8', 'II9', 'V1', 'V2', 'V21', 'V22', 'V3', 'V5', 'V6', 'V7', 'V8', 'V11', 'V12', 'V13', 'V14', 'IX_TOT', 'IX_MEN10', 'IX_MAYEQ10', 'ITF', 'DECCFR'
 ]
 
 cols_id_individual = [
-    'CODUSU','NRO_HOGAR', 'COMPONENTE', 'H15', 'ANO4', 'TRIMESTRE',  'REGION','MAS_500', 'AGLOMERADO', 'PONDERA'
+    'CODUSU', 'NRO_HOGAR', 'COMPONENTE', 'H15', 'ANO4', 'TRIMESTRE',  'REGION', 'MAS_500', 'AGLOMERADO', 'PONDERA'
 ]
 
 cols_individual = [
-    'CH03', 'CH04', 'CH06', 'CH07', 'CH08', 'CH09', 'CH10', 'CH11', 'CH15', 'CH16', 'ESTADO', 'CAT_OCUP', 'CAT_INAC', 'PP02E', 'PP02H', 'DECINDR', 'T_VI'
+    'CH03', 'CH04', 'CH06', 'CH07', 'CH08', 'CH09', 'CH10', 'CH11', 'CH15', 'CH16', 'ESTADO', 'CAT_OCUP', 'CAT_INAC', 'PP02E', 'PP02H', 'PP07I', 'PP07H', 'PP04B1', 'DECINDR', 'T_VI', ''
 ]
 
-def seleccionar_features(data, 
-                         lista_columnas: list, 
+
+def seleccionar_features(data,
+                         lista_columnas: list,
                          cols_id: list = None):
     """Selecciona una lista de columnas de la EPH.
 
@@ -33,8 +34,9 @@ def seleccionar_features(data,
         cols_id = []
     return data[cols_id + lista_columnas]
 
-def filtrar_por_columnas(data: pd.DataFrame, 
-                         filtro: str, 
+
+def filtrar_por_columnas(data: pd.DataFrame,
+                         filtro: str,
                          lista_columnas: list = None) -> pd.DataFrame:
     """Filtra una base de datos según los filtros pasados como argumento usando el método query de pandas. Devuelve la base filtrada con las columnas requeridas por lista_columnas.
 
@@ -47,13 +49,14 @@ def filtrar_por_columnas(data: pd.DataFrame,
         pd.DataFrame: _description_
     """
     if lista_columnas is None:
-        lista_columnas=data.columns
+        lista_columnas = data.columns
     data_filtrada = data.query(filtro)[lista_columnas]
     return data_filtrada
 
-def eliminar_duplicados(base_datos:pd.DataFrame, lista_bases_comparar:list)->pd.DataFrame:
+
+def eliminar_duplicados(base_datos: pd.DataFrame, lista_bases_comparar: list) -> pd.DataFrame:
     """Elimina los individuos duplicados entre la base de datos y las bases de datos de la lista.
-    
+
     Parámetros:
         base_datos (pd.DataFrame): Base de datos principal que se va a comparar y modificar.
         lista_bases_comparar (list): Lista de bases de datos que se van a usar para eliminar duplicados.
@@ -65,7 +68,7 @@ def eliminar_duplicados(base_datos:pd.DataFrame, lista_bases_comparar:list)->pd.
 
     for otras_bd in lista_bases_comparar:
         base_depurada = base_depurada.merge(otras_bd[['CODUSU', 'NRO_HOGAR', 'COMPONENTE']],
-                                                how='left', indicator=True).\
-                                query('_merge == "left_only"').drop(columns=['_merge'])
+                                            how='left', indicator=True).\
+            query('_merge == "left_only"').drop(columns=['_merge'])
 
     return base_depurada

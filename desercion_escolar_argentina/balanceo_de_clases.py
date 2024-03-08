898066a1aed4572ec24778676f981c8e668941f2
Lucianoaleg <lleguiza@fi.uba.ar>
2024-03-08 20:24:26 -0300

    balanceo_de_clases: Se agregaron los metodos SMOTE, ADASYN para Oversampling y los metodos RandomUnderSampler y AllKNN para undersampling en la iteracion que da muestras de 0.2 a 0.5 porciento de la muestra minoritaria sobre la mayoritaria pasando de a 0.05 porciento

--- desercion_escolar_argentina/balanceo_de_clases.py
+++ desercion_escolar_argentina/balanceo_de_clases.py
@@ -1,8 +1,8 @@
 import pandas
-from sklearn.linear_model import LogisticRegression
 from imblearn.over_sampling import SMOTE, ADASYN
 from imblearn.over_sampling import RandomOverSampler
-from collections import Counter
+from imblearn.under_sampling import RandomUnderSampler
+from imblearn.under_sampling import AllKNN
 
 # Ver head del preprocessed.csv
 df = pandas.read_csv("data/preprocessed/preprocessed_dataset.csv")
@@ -17,28 +17,98 @@ print(variables_explicativas_df)
 variable_explicada_df = df.loc[:, "DESERTO"]
 print(variable_explicada_df)
 
+# Crear el objeto SMOTE variable_explicada_df ADASYN
+smote = SMOTE()
+adasyn = ADASYN()
+allknn = AllKNN()
 # Hago loop con minoritaria a 0.2 hasta 0.5 de la mayoritaria
 
 muestra_minoritaria = 0.2
 muestra_mayoritaria = 0.8
 
 for i in range(7):
-
+    # Establezco la sampling strategy
     sampling_strategy = round(muestra_minoritaria / muestra_mayoritaria, 2)
     print(sampling_strategy)
-    # Probamos primero con un Oversampling estandar
 
+    # Probamos primero con un Oversampling estandar
     df_resampled_oversampled, variable_explicada_df_resampled = RandomOverSampler(
         sampling_strategy=sampling_strategy
     ).fit_resample(df, variable_explicada_df)
     df_resampled_oversampled = pandas.DataFrame(df_resampled_oversampled)
 
-    # df_resampled_oversampled.assign(variable_explicada_df_resampled)
+    # Printeo el head para ver como se guardará
     print(df_resampled_oversampled.head())
 
-    # print(sorted(Counter(variable_explicada_df_resampled).items()))
+    # Guardo como CSV cada muestra resampleada con el método correspondiente
     df_resampled_oversampled.to_csv(
         f"./data/stage/df_resampled_oversampled_{round(muestra_minoritaria,2)}.csv",
+        sep=",",
+        index=False,
+        encoding="utf-8",
+    )
+    # Aplicamos Metodo SMOTE
+    df_resampled_SMOTE, variable_explicada_df_SMOTE = smote.fit_resample(
+        sampling_strategy=sampling_strategy
+    ).fit_resample(df, variable_explicada_df)
+    df_resampled_SMOTE = pandas.DataFrame(df_resampled_SMOTE)
+
+    # Printeo el head para ver como se guardará
+    print(df_resampled_SMOTE.head())
+
+    # Guardo como CSV cada muestra resampleada con el método correspondiente
+    df_resampled_SMOTE.to_csv(
+        f"./data/stage/df_resampled_SMOTE_{round(muestra_minoritaria,2)}.csv",
+        sep=",",
+        index=False,
+        encoding="utf-8",
+    )
+
+    # Aplicamos Metodo ADASYN
+    df_resampled_ADASYN, variable_explicada_df_ADASYN = adasyn.fit_resample(
+        sampling_strategy=sampling_strategy
+    ).fit_resample(df, variable_explicada_df)
+    df_resampled_ADASYN = pandas.DataFrame(df_resampled_ADASYN)
+
+    # Printeo el head para ver como se guardará
+    print(df_resampled_ADASYN.head())
+
+    # Guardo como CSV cada muestra resampleada con el método correspondiente
+    df_resampled_ADASYN.to_csv(
+        f"./data/stage/df_resampled_ADASYN_{round(muestra_minoritaria,2)}.csv",
+        sep=",",
+        index=False,
+        encoding="utf-8",
+    )
+    # Probamos primero con un Undersampling estandar
+    df_resampled_undersampled, variable_explicada_df_undersampled = RandomUnderSampler(
+        sampling_strategy=sampling_strategy
+    ).fit_resample(df, variable_explicada_df)
+    df_resampled_undersampled = pandas.DataFrame(df_resampled_undersampled)
+
+    # Printeo el head para ver como se guardará
+    print(df_resampled_undersampled.head())
+
+    # Guardo como CSV cada muestra resampleada con el método correspondiente
+    df_resampled_undersampled.to_csv(
+        f"./data/stage/df_resampled_undersampled_{round(muestra_minoritaria,2)}.csv",
+        sep=",",
+        index=False,
+        encoding="utf-8",
+    )
+
+    # Probamos primero con un Undersampling AllKNN
+    df_resampled_AllKNN, variable_explicada_df_AllKNN = allknn(
+        sampling_strategy=sampling_strategy
+    ).fit_resample(df, variable_explicada_df)
+    df_resampled_AllKNN = pandas.DataFrame(df_resampled_AllKNN)
+
+    # Printeo el head para ver como se guardará
+    print(df_resampled_AllKNN.head())
+
+    # Guardo como CSV cada muestra resampleada con el método correspondiente
+    df_resampled_AllKNN.to_csv(
+        f"./data/stage/df_resampled_AllKNN_{round(muestra_minoritaria,2)}.csv",
         sep=",",
         index=False,
         encoding="utf-8",

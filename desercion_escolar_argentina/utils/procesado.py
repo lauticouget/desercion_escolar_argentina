250bf476f7c17619d29a2ed564e473d6e0aa03ed
jbeiroa <jbeiroa@gmail.com>
2024-03-15 15:34:20 -0300

    train_validate_test: procesado.py

---
+++ desercion_escolar_argentina/utils/procesado.py
@@ -0,0 +1,8 @@
+# dropear columnas por alta correlacion/innecesarias
+drop_cols = [
+    'IV8', 'IX_MAYEQ', 'CAT_OCUP', 'CAT_INAC', 'CAT_OCUP_jefx', 'JEFE_TRABAJA',
+    ''
+]
+# homogeneizar binarias
+# catnom a dummies
+# 

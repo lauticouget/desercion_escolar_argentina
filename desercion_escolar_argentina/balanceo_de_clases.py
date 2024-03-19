47327bd940a2180c38e7a805b00a50409fae6201
laucouget <lautaro.couget@kenility.com>
2024-03-19 18:52:47 -0300

    develop: balanceo completo

--- desercion_escolar_argentina/balanceo_de_clases.py
+++ desercion_escolar_argentina/balanceo_de_clases.py
@@ -1,16 +1,15 @@
-import os 
-
+import os
 import pandas
-from imblearn.over_sampling import RandomOverSampler
-from imblearn.under_sampling import RandomUnderSampler
-
+from imblearn.over_sampling import RandomOverSampler, SMOTE, ADASYN
+from imblearn.under_sampling import RandomUnderSampler, AllKNN
 from desercion_escolar_argentina.utils import file_handler as fh
+
 # preprocessed_train
 
 repo_path = fh.get_repo_path()
-pr_path = os.path.join(repo_path, 'data/preprocessed/')
-stage_path = os.path.join(repo_path, 'data/stage/')
-train_path = os.path.join(pr_path, 'preprocessed_train.csv')
+pr_path = os.path.join(repo_path, "data/preprocessed/")
+stage_path = os.path.join(repo_path, "data/stage/")
+train_path = os.path.join(pr_path, "preprocessed_train.csv")
 df = pandas.read_csv(train_path)
 
 
@@ -33,7 +32,42 @@ for i in range(7):
 
     # Guardo como CSV cada muestra resampleada con el método correspondiente
     df_resampled_oversampled.to_csv(
-        os.path.join(stage_path, f"train_oversampled_{round(muestra_minoritaria,2)}.csv"),
+        os.path.join(
+            stage_path, f"train_oversampled_{round(muestra_minoritaria,2)}.csv"
+        ),
+        sep=",",
+        index=False,
+        encoding="utf-8",
+    )
+    # Aplicamos Metodo SMOTE
+    df_resampled_SMOTE, variable_explicada_df_SMOTE = SMOTE(
+        sampling_strategy=sampling_strategy
+    ).fit_resample(variables_explicativas_df, variable_explicada_df)
+    df_resampled_SMOTE = pandas.DataFrame(df_resampled_SMOTE)
+
+    # Printeo el head para ver como se guardará
+    print(df_resampled_SMOTE.head())
+
+    # Guardo como CSV cada muestra resampleada con el método correspondiente
+    df_resampled_SMOTE.to_csv(
+        f"data/stage/df_resampled_SMOTE_escalado_{round(muestra_minoritaria,2)}.csv",
+        sep=",",
+        index=False,
+        encoding="utf-8",
+    )
+
+    # Aplicamos Metodo ADASYN
+    df_resampled_ADASYN, variable_explicada_df_ADASYN = ADASYN(
+        sampling_strategy=sampling_strategy
+    ).fit_resample(variables_explicativas_df, variable_explicada_df)
+    df_resampled_ADASYN = pandas.DataFrame(df_resampled_ADASYN)
+
+    # Printeo el head para ver como se guardará
+    print(df_resampled_ADASYN.head())
+
+    # Guardo como CSV cada muestra resampleada con el método correspondiente
+    df_resampled_ADASYN.to_csv(
+        f"data/stage/df_resampled_ADASYN_escalado_{round(muestra_minoritaria,2)}.csv",
         sep=",",
         index=False,
         encoding="utf-8",
@@ -47,7 +81,9 @@ for i in range(7):
 
     # Guardo como CSV cada muestra resampleada con el método correspondiente
     df_resampled_undersampled.to_csv(
-        os.path.join(stage_path, f"train_undersampled_{round(muestra_minoritaria,2)}.csv"),
+        os.path.join(
+            stage_path, f"train_undersampled_{round(muestra_minoritaria,2)}.csv"
+        ),
         sep=",",
         index=False,
         encoding="utf-8",
@@ -55,3 +91,21 @@ for i in range(7):
 
     muestra_minoritaria = muestra_minoritaria + 0.05
     muestra_mayoritaria = muestra_mayoritaria - 0.05
+
+
+# Probamos con un Undersampling AllKNN
+df_resampled_AllKNN, variable_explicada_df_AllKNN = AllKNN().fit_resample(
+    variables_explicativas_df, variable_explicada_df
+)
+df_resampled_AllKNN = pandas.DataFrame(df_resampled_AllKNN)
+
+# Printeo el head para ver como se guardará
+print(df_resampled_AllKNN.head())
+
+# Guardo como CSV cada muestra resampleada con el método correspondiente
+df_resampled_AllKNN.to_csv(
+    f"data/stage/df_resampled_AllKNN_escalado.csv",
+    sep=",",
+    index=False,
+    encoding="utf-8",
+)
